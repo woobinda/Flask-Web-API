9ef22ca77606370bec97ae20ea0e1a37737d6134
woobinda <woobinda@voliacable.com>
2016-06-05 05:42:58 +0300

    fix

--- app.py
+++ app.py
@@ -1,17 +1,17 @@
 # -*- coding: utf-8 -*- 
-from flask import Flask, render_template, session, redirect, url_for, json
-from flask_bootstrap import Bootstrap
-from flask_wtf import Form
-from wtforms import TextAreaField, SubmitField, IntegerField, SelectField, StringField
-from wtforms.validators import Required, Regexp
-from datetime import datetime
-from flask_sqlalchemy import SQLAlchemy
-from flask_script import Shell, Manager
-import os
 import hashlib
-import requests
 import logging
+import os
+import requests
+from datetime import datetime
 
+from flask import Flask, render_template, json
+from flask_bootstrap import Bootstrap
+from flask_script import Manager
+from flask_sqlalchemy import SQLAlchemy
+from flask_wtf import Form
+from wtforms import TextAreaField, SubmitField, IntegerField, SelectField
+from wtforms.validators import Required
 
 app = Flask(__name__)
 basedir = os.path.abspath(os.path.dirname(__file__))
@@ -64,67 +64,76 @@ def _get_sign(request, keys_required, secret):
 @app.route('/', methods=['GET', 'POST'])
 def index():
     form = PayForm()
-
     if form.validate_on_submit():
-        logging.info('---Start---')
         created_date = datetime.now()
         order = Order(form.amount.data, form.currency.data, form.description.data, created_date)
         db.session.add(order)
         db.session.commit()
-        app.logger.info('Create a order (id %s)' % order.id)
+        app.logger.info('Create a order id %s' % order.id)
 
         if form.currency.data == 'card_rub':
-            logging.info('Starting redirect')
+            app.logger.info("Payway is '%s'" % form.currency.data)
             request = dict(shop_id=shop_id, amount=form.amount.data, shop_invoice_id=order.id,
                            currency=currencys[form.currency.data])
-            app.logger.debug('Choicen payway is ( %s)' % form.currency.data)
             keys_required = ("shop_id", "amount", "currency", "shop_invoice_id")
             sign = _get_sign(request, keys_required, secret)
-            app.logger.info('Generate a HTML form')
-            return render_template('payform_rub.html', amount=str(form.amount.data),
-                                   currency=str(currencys[form.currency.data]), shop_id=str(shop_id),
-                                   shop_invoice_id=str(order.id), sign=str(sign),
+            app.logger.info('Generate a HTML form for redirect')
+            return render_template('payform_rub.html',
+                                   amount=str(form.amount.data),
+                                   currency=str(currencys[form.currency.data]),
+                                   shop_id=str(shop_id),
+                                   shop_invoice_id=str(order.id),
+                                   sign=str(sign),
                                    description=str(form.description.data),
                                    failed_url="https://tip.pay-trio.com/failed/",
                                    success_url="https://tip.pay-trio.com/success/")
 
         if form.currency.data == 'w1_uah':
-            logging.info('---Start---')
-            request = dict(amount=form.amount.data, currency=currencys[form.currency.data],
-                           payway=form.currency.data, shop_id=shop_id, shop_invoice_id=order.id)
-            app.logger.info('Currency is ( %s)' % form.currency.data)
+            app.logger.info("Payway is '%s'" % form.currency.data)
+            request = dict(amount=form.amount.data,
+                           currency=currencys[form.currency.data],
+                           payway=form.currency.data,
+                           shop_id=shop_id,
+                           shop_invoice_id=order.id)
             keys_required = ("amount", "currency", "payway", "shop_id", "shop_invoice_id")
             sign = _get_sign(request, keys_required, secret)
-            request['sign'] = sign
-            request['description'] = form.description.data
+            request["sign"] = sign
+            request["description"] = form.description.data
             url = "https://central.pay-trio.com/invoice"
             headers = {'Content-type': 'application/json'}
             request_to_api = requests.post(url, data=json.dumps(request), headers=headers)
             response_from_api = json.loads(request_to_api.text)
-            app.logger.debug('Response_from_api result is ( %s)' % response_from_api['result'])
+            app.logger.info('API Response result is "%s"' % response_from_api['result'])
+
             if response_from_api['result'] == 'ok':
                 data = response_from_api['data']['data']
-                app.logger.info('Generate a HTML form')
-                return render_template('payform_uah.html', WMI_CURRENCY_ID=str(data['WMI_CURRENCY_ID']),
-                                       WMI_FAIL_URL=str(data['WMI_FAIL_URL']),
-                                       WMI_MERCHANT_ID=str(data['WMI_MERCHANT_ID']),
-                                       WMI_PAYMENT_AMOUNT=str(data['WMI_PAYMENT_AMOUNT']),
-                                       WMI_PAYMENT_NO=str(data['WMI_PAYMENT_NO']),
-                                       WMI_PTENABLED=str(data['WMI_PTENABLED']),
-                                       WMI_SIGNATURE=str(data['WMI_SIGNATURE']),
-                                       WMI_SUCCESS_URL=str(data['WMI_SUCCESS_URL']))
-    else:
-        app.logger.debug('Validation Form Error')
+                app.logger.info('Generate a HTML form for redirect')
+                return render_template('payform_uah.html',
+                                       WMI_CURRENCY_ID=str(data["WMI_CURRENCY_ID"]),
+                                       WMI_FAIL_URL=str(data["WMI_FAIL_URL"]),
+                                       WMI_MERCHANT_ID=str(data["WMI_MERCHANT_ID"]),
+                                       WMI_PAYMENT_AMOUNT=str(data["WMI_PAYMENT_AMOUNT"]),
+                                       WMI_PAYMENT_NO=str(data["WMI_PAYMENT_NO"]),
+                                       WMI_PTENABLED=str(data["WMI_PTENABLED"]),
+                                       WMI_SIGNATURE=str(data["WMI_SIGNATURE"]),
+                                       WMI_SUCCESS_URL=str(data["WMI_SUCCESS_URL"]))
+            else:
+                app.logger.error("API Response result is '%s'" % response_from_api['result'])
+                return render_template('500.html')
     return render_template('payform_index.html', form=form)
 
 
 @app.errorhandler(404)
 def page_not_found(e):
-    app.logger.debug('404 Not Found')
     return render_template('404.html'), 404
 
 
+@app.errorhandler(500)
+def internal_server_error(e):
+    app.logger.error('Error 500, Internal Server Error')
+    return render_template('500.html'), 500
+
+
 if __name__ == '__main__':
-    logging.basicConfig(filename='app.log', filemode='w', level=logging.DEBUG)
-    logging.debug('This message should go to the log file')
-    app.run(debug=True)
+    logging.basicConfig(filename='app.log', filemode='w', asctime=True, level=logging.DEBUG)
+    app.run()
